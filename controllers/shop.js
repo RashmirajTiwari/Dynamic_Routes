e4bcb9dee4a9f3e23f3f893f7a3c45db0eb89177
Rashmiraj Tiwari <rashmirajt@athmin.com>
2023-08-08 10:53:19 +0530

    Worked on cart to add cart data and fetch cart related data

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -34,6 +35,14 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+exports.postCart = (req, res, next) => {
+  const proId=req.body.productId;
+  Product.findById(proId,(product)=>{
+    Cart.addCart(proId,product.price)
+  })
+  res.redirect('/cart');
+};
+
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
